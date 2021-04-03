c44e4856ee524e074b14fafbf3abbc34ef90a2a5
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2021-04-04 02:55:08 +0900

    反転させる関数としてまとめました
    まとめるって大切

--- 3-4/main.py
+++ 3-4/main.py
@@ -2,30 +2,27 @@ import sys
 import numpy as np
 
 
+def reverse_a_number(arg):
+    num = []
+    for i in arg:
+        # １文字ずつ配列に格納
+        num.append(i)
+        # 配列を反転
+        renum = num[::-1]
+    # 配列の形を崩す
+    result = ''.join(renum)
+    # 数値へ変換
+    result = int(result)
+    return(result)
+
+
 def main():
     # 入力は文字列扱い
     a = sys.argv[1]
     b = sys.argv[2]
-    num1 = []
-    num2 = []
-    for i in a:
-        # １文字ずつ配列に格納
-        num1.append(i)
-        # 配列を反転
-        renum1 = num1[::-1]
-    # 配列の形を崩す
-    result1 = ''.join(renum1)
-    # 数値へ変換
-    result1 = int(result1)
 
-    for j in b:
-        num2.append(j)
-        renum2 = num2[::-1]
-    result2 = ''.join(renum2)
-    result2 = int(result2)
-
-    result = result1 + result2
-    print(result)
+    results = reverse_a_number(a) + reverse_a_number(b)
+    print(results)
 
 
 if __name__ == '__main__':
